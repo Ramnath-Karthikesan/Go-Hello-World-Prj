844a0e4135ff4e7afede9c72b3011514a746971d
Ramnath Karthikesan <ramnathkarthikesan@gmail.com>
2022-11-23 13:13:27 +0530

    added session functionality

--- cmd/pkg/handlers/handlers.go
+++ cmd/pkg/handlers/handlers.go
@@ -34,6 +34,9 @@ func NewHandlers(r *Repository) {
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "This is the home page")
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "index.page.tmpl", &models.TemplateData{})
 }
 
@@ -45,6 +48,11 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	//perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	remoteIP :=  m.App.Session.GetString(r.Context(), "remote_ip")
+
+	
+	stringMap["remote_ip"] = remoteIP
 	//send data to template
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
